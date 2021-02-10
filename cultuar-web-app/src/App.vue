ac6a64f4d5e13f33522ae54bd16e33cac48efbe1
Valentín Pedrosa <valentin@beebit.es>
2021-02-10 14:57:42 +0100

    CU-e2y267 - Diseño de aplicación en Vue.js para parametrización

---
+++ cultuar-web-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ModelViewer
+    slug="alhambra/alhambra-v.1.0"
+    description="Modelo 3D de la Alhambra"
+  ></ModelViewer>
+</template>
+
+<script>
+import ModelViewer from "./components/ModelViewer.vue";
+
+export default {
+  name: "App",
+  components: {
+    ModelViewer,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cultuar-web-app/src/components/ModelViewer.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script>
+export default {
+  name: "ModelViewer",
+  route: "assets/3d/",
+  props: {
+    slug: String,
+    description: String,
+  },
+};
+let start = "<model-viewer ";
+let src = 'src="' + route + slug + '.glb"';
+let iosSrc = 'ios-src="' + route + slug + '.glb"';
+let description = 'description="' + description + '"';
+let end = ``` auto-rotate
+    ar
+    ar-modes="scene-viewer quick-look webxr"
+    ar-scale="auto"
+    camera-controls
+  ></model-viewer>```;
+
+let model = start + src + iosSrc + end;
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
